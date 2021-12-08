c636ce8855f741a4c81ec56386a36b772a696987
diegocastillosaldana <diegocastillocs98@gmail.com>
2021-12-08 16:23:54 -0500

    modificando menu nav

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="titleMerc">
-          Tienda virtual - Rincón Añejo
-        </p>
-        <span>Un gusto para tu paladar</span>
-        <br />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NavBar/>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import logorincon from '../logorincon.png';
+import carrito from '../carrito.svg';
+import flecha from '../flacha.svg';
+import menu from '../menu';
+import barramenu from '../menu.svg';
+import '../App.css';
 
 const NavBar = () => {
     return (
-        <h3>Barra de Navegación</h3>
+        <header className="header">
+        <div className="container logo-nav-container">
+            <a href="#inicio" className="logo"><img src={logorincon} alt="logo"/></a>
+            <nav class="navigation">
+                <span className="nav-bar" id="btnMenu"><img src={barramenu} alt="menu"/></span>
+                <ul className="menu">
+                    <li className="menu-item container-submenu"><a className="hvr-underline-from-center" href="#Vinos">Vinos<img className="flecha" src={flecha} alt="flecha"/></a>
+                        <ul className="submenu">
+                            <li className="submenu-item"><a className="hvr-underline-from-center" href="#Tintos">Tintos</a></li>
+                            <li className="submenu-item"><a className="hvr-underline-from-center" href="#Blancos">Blancos</a></li>
+                        </ul>
+                    </li>
+                    <li className="menu-item container-submenu"><a className="hvr-underline-from-center" href="#Pisco">Pisco<img className="flecha" src={flecha} alt="flecha"/></a>
+                         <ul className="submenu">
+                            <li className="submenu-item"><a className="hvr-underline-from-center" href="#Tintos">Puro</a></li>
+                            <li className="submenu-item"><a className="hvr-underline-from-center" href="#Blancos">Acholado</a></li>
+                        </ul> 
+                    </li>
+                    <li className="menu-item"><a className="hvr-underline-from-center" href="#Promociones">Promociones</a></li>
+                    <li className="menu-item li-carrito"><a className="hvr-underline-from-center" href="#carrito"><img src={carrito} className="carrito" alt="carrito"/></a></li>
+                </ul>
+            </nav>
+        </div>
+        <script src={menu}></script>
+        </header>        
     )
 }
 
